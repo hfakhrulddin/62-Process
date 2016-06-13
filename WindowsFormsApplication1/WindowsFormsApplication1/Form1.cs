4ce63369aa98e137326b5e6dfb289caca89517a7
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-12 22:16:17 -0700

    init

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Process p in Process.GetProcesses())
+                p.Kill();
+
+
+
+            Process.Start("cmd.exe");//system 32 folder programs.//1
+            MessageBox.Show(Process.GetCurrentProcess().ProcessName);//2
+            OpenFileDialog ofd = new OpenFileDialog();//3
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start(ofd.FileName);
+
+
+            }
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
